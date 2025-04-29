a8f724d230a3c989c5f08c8accaa2fd85b236f36
Shilo <shiloworks@gmail.com>
2025-04-29 18:52:26 +1000

    Remove unused formControl property from FormCustomFieldModel

--- projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.model.ts
+++ projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.model.ts
@@ -2,5 +2,5 @@ import { FormControl } from '@angular/forms';
 import { FormFieldModel } from '@researchdatabox/portal-ng-common';
 
 export class FormCustomFieldModel extends FormFieldModel<undefined> {
-  public override formControl: FormControl<undefined | null> = new FormControl<undefined | null>(undefined);
+
 }
