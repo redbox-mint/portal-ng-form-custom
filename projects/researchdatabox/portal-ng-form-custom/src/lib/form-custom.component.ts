9b2987da1f22fb184f154b20784701594ed0395a
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2025-07-18 16:19:28 +1000

    add example custom form config and types

--- projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.component.ts
+++ projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormFieldBaseComponent } from '@researchdatabox/portal-ng-common';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from "@angular/forms";
+import {FormCustomFieldModel, FormCustomFieldModelValueType} from "./form-custom.model";
 
 @Component({
   selector: 'redbox-custom-sample-component',
@@ -8,13 +10,20 @@ import { CommonModule } from '@angular/common';
     <p>
       ReDBox Custom Sample Component works!
     </p>
+    @if (getBooleanProperty('visible')) {
+      <ng-container *ngTemplateOutlet="getTemplateRef('before')"/>
+      <input type='number' [formControl]="formControl"/>
+      <ng-container *ngTemplateOutlet="getTemplateRef('after')"/>
+    }
   `,
   styles: [
   ],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class FormCustomComponent extends FormFieldBaseComponent<undefined> {
+export class FormCustomComponent extends FormFieldBaseComponent<FormCustomFieldModelValueType> {
+  @Input() public override model?: FormCustomFieldModel;
+  protected override logName: string = "ExampleCustomFieldComponent";
 }
 
 
--- projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.model.ts
+++ projects/researchdatabox/portal-ng-form-custom/src/lib/form-custom.model.ts
@@ -1,6 +1,132 @@
-import { FormControl } from '@angular/forms';
-import { FormFieldModel } from '@researchdatabox/portal-ng-common';
+import {FormFieldModel} from '@researchdatabox/portal-ng-common';
+import {
+  BaseFormFieldComponentConfig,
+  BaseFormFieldComponentDefinition,
+  BaseFormFieldModelConfig,
+  BaseFormFieldModelDefinition,
+  FormComponentDefinition,
+  FormConfig,
+  FormFieldComponentDefinition,
+  FormFieldModelDefinition
+} from '@researchdatabox/sails-ng-common';
 
-export class FormCustomFieldModel extends FormFieldModel<undefined> {
+export type FormCustomFieldModelValueType = number;
 
+export class FormCustomFieldModel extends FormFieldModel<FormCustomFieldModelValueType> {
+
+}
+
+export interface ExampleCustomFormFieldComponentDefinition extends BaseFormFieldComponentDefinition {
+  class: "ExampleCustomFieldComponent";
+  config?: ExampleCustomFormFieldComponentConfig;
+}
+
+export class ExampleCustomFormFieldComponentConfig extends BaseFormFieldComponentConfig {
+}
+
+export interface ExampleCustomFormFieldModelDefinition extends BaseFormFieldModelDefinition<FormCustomFieldModelValueType> {
+  class: "ExampleCustomFieldModel";
+  config: ExampleCustomFormFieldModelConfig;
+}
+
+export class ExampleCustomFormFieldModelConfig extends BaseFormFieldModelConfig<FormCustomFieldModelValueType> {
+
+}
+
+
+export type AllExampleCustomFormFieldModelDefinition = FormFieldModelDefinition | ExampleCustomFormFieldModelDefinition;
+export type AllExampleCustomFormFieldComponentDefinition =
+  FormFieldComponentDefinition
+  | ExampleCustomFormFieldComponentDefinition;
+
+
+export interface ExampleCustomFormComponentDefinition extends Omit<FormComponentDefinition, 'model' | "component"> {
+  /**
+   * The definition of the model that backs the form field.
+   */
+  model: AllExampleCustomFormFieldModelDefinition;
+  /**
+   * The definition of the client-side component for the form field.
+   */
+  component: AllExampleCustomFormFieldComponentDefinition;
+}
+
+// Can't use this because typescript class mixins are not supported by the json schema generator
+// /**
+//  * Use the built-in TypeScript 'Omit' to remove properties from a class.
+//  * @param Class The class to operate on.
+//  * @param keys The properties to remove.
+//  * @constructor
+//  */
+// const OmitClass = <T, K extends keyof T>(Class: new () => T, keys: K[]): new () => Omit<T, typeof keys[number]> => Class;
+
+
+
+/**
+ * The customised FormConfig can come from a sails hook, or a separate repo,
+ *   or from custom angular components, and just import the custom form config.
+ *
+ *
+ * This is needed to ensure the form config declaration can use the custom components.
+ *
+ *
+ * Note that this is an interface, the 'FormConfig' is a class.
+ * That shouldn't matter, the interface is only used for generating the json schema and type checking.
+ *
+ *
+ * WARNING: The way this is done means that the CustomFormConfig and FormConfig are not compatible,
+ * but they are used for typescript type checking and building json schema,
+ * so the incompatibility doesn't matter.
+ */
+export interface CustomFormConfig extends Omit<FormConfig, 'componentDefinitions'> {
+  componentDefinitions?: ExampleCustomFormComponentDefinition[];
+}
+
+
+export const exampleCustomFormConfig: CustomFormConfig = {
+  name: "example-1.0-draft",
+  type: "rdmp",
+  debugValue: true,
+  domElementType: 'form',
+  defaultComponentConfig: {
+    defaultComponentCssClasses: 'row',
+  },
+  editCssClasses: "redbox-form form",
+  skipValidationOnSave: false,
+  validatorDefinitions: [],
+  validators: [],
+  componentDefinitions: [
+    {
+      name: 'core_text_1',
+      model: {
+        class: 'TextFieldModel',
+        config: {
+          value: 'hello world!',
+          defaultValue: 'hello world!',
+          validators: [
+            {name: 'required'},
+          ]
+        }
+      },
+      component: {
+        class: 'TextFieldComponent'
+      }
+    },
+    {
+      name: 'example_custom_1',
+      model: {
+        class: 'ExampleCustomFieldModel',
+        config: {
+          value: 3,
+          defaultValue: 10,
+          validators: [
+            {name: 'required'},
+          ]
+        }
+      },
+      component: {
+        class: 'ExampleCustomFieldComponent'
+      }
+    }
+  ]
 }
